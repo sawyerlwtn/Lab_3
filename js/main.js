35d15631375d393886bd67f9c969138684351d10
sawyerlwtn <sawyerlawton10@gmail.com>
2026-02-11 12:37:20 -0800

    Created Choropleth Theme & Legend

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([41.2, -95.9], 5);
+var map = L.map('map').setView([41.2, -95.9], 4);
 var dataStats = {}; 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -160,9 +160,136 @@ function createLegend() {
 //<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\\
 //<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\\
 //establish variable for choropleth map (map2)
-var map2 = L.map('map2').setView([41.2, -95.9], 5);
+var map2 = L.map('map2').setView([41.2, -95.9], 4);
 //create a basemap for the choropleth map
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map2);
+
+// 1. Define the color scale function for the Choropleth
+function getChoroColor(d) {
+    // Colors based on total city population within the state
+    return d > 20000000 ? '#084594' :
+           d > 10000000 ? '#2171b5' :
+           d > 5000000  ? '#4292c6' :
+           d > 1000000  ? '#6baed6' :
+           d > 500000   ? '#9ecae1' :
+                          '#eff3ff';
+}
+
+// 2. Fetch both the US States (polygons) and your Cities (points)
+Promise.all([
+    // Public US States GeoJSON for the areal units
+    fetch('https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json').then(res => res.json()),
+    fetch('data/USA_Major_Cities/USA_Major_Cities.json').then(res => res.json())
+]).then(function(data) {
+    var statesData = data[0];
+    var citiesData = data[1];
+
+    // 3. Perform the Spatial Join using Turf.js
+    statesData.features.forEach(function(state) {
+        var statePopSum = 0;
+
+        citiesData.features.forEach(function(city) {
+            // Check if the city point falls inside the state polygon
+            if (turf.booleanPointInPolygon(city, state)) {
+                statePopSum += Number(city.properties.POPULATION);
+            }
+        });
+
+        // Add the calculated sum as a new property to the state
+        state.properties.TOTAL_CITY_POP = statePopSum;
+    });
+
+    // 4. Add the joined data to map2
+   // Add the joined data to map2
+    L.geoJson(statesData, {
+        style: function(feature) {
+            return {
+                fillColor: getChoroColor(feature.properties.TOTAL_CITY_POP),
+                weight: 1.5,
+                opacity: 1,
+                color: 'white',
+                fillOpacity: 0.8
+            };
+        },
+        // This is where the 'layer.on' magic happens
+        onEachFeature: function (feature, layer) {
+            var total = feature.properties.TOTAL_CITY_POP ? feature.properties.TOTAL_CITY_POP.toLocaleString() : '0';
+            var popupContent = '<h1>' + feature.properties.name + '</h1>' + 
+                               '<p>Total Urban Pop: ' + total + '</p>';
+
+            // 1. Hover 'In' Event
+            layer.on('mouseover', function (e) {
+                // We use 'this' to refer to the specific state layer being hovered
+                this.bindPopup(popupContent, { 
+                    closeButton: false, 
+                    offset: L.point(0, -10) 
+                }).openPopup();
+                
+                // Visual highlight
+                this.setStyle({
+                    weight: 3,
+                    color: '#333',
+                    fillOpacity: 0.9
+                });
+            });
+
+            // 2. Hover 'Out' Event
+            layer.on('mouseout', function (e) {
+                this.closePopup();
+                
+                // Reset to the original style
+                this.setStyle({
+                    weight: 1.5,
+                    color: 'white',
+                    fillOpacity: 0.8
+                });
+            });
+        }
+    }).addTo(map2);
+
+    // 5. Add a Title specifically for map2
+    createChoroTitle(map2);
+    createChoroLegend(map2);
+});
+
+// Separate Title function for map2 (No box, centered text)
+function createChoroTitle(mapInstance) {
+    var TitleControl = L.Control.extend({
+        options: { position: 'topleft' },
+        onAdd: function () {
+            var container = L.DomUtil.create('div', 'map-title-container');
+            container.innerHTML = '<h1>State-wide Urban Population</h1>';
+            return container;
+        }
+    });
+    mapInstance.addControl(new TitleControl());
+};
+//create a legend lil bro
+function createChoroLegend(mapInstance) {
+    var LegendControl = L.Control.extend({
+        options: { position: 'bottomright' },
+        onAdd: function () {
+            // Create the container using the same class as map1 for consistency
+            var container = L.DomUtil.create('div', 'legend-control-container');
+            container.innerHTML = '<b>Total Urban Pop</b><br>';
+
+            // Define the breaks and colors to match your getChoroColor function
+            var grades = [0, 500000, 1000000, 5000000, 10000000, 20000000];
+            var labels = [];
+
+            // Loop through our density intervals and generate a label with a colored square for each interval
+            for (var i = 0; i < grades.length; i++) {
+                container.innerHTML +=
+                    '<i style="background:' + getChoroColor(grades[i] + 1) + '; width: 18px; height: 18px; float: left; margin-right: 8px; opacity: 0.8;"></i> ' +
+                    (grades[i] >= 1000000 ? (grades[i] / 1000000) + 'M' : (grades[i] / 1000) + 'k') + 
+                    (grades[i + 1] ? '&ndash;' + (grades[i + 1] >= 1000000 ? (grades[i + 1] / 1000000) + 'M' : (grades[i + 1] / 1000) + 'k') + '<br>' : '+');
+            }
+
+            return container;
+        }
+    });
+    mapInstance.addControl(new LegendControl());
+}
